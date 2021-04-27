4153ad8a2803da532d6cb87643eb6c47f16fa429
Gaurang-1 <72252176+Gaurang-1@users.noreply.github.com>
2021-04-27 13:09:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+status="";
+
 function setup(){
     canvas=createCanvas(360,360);
     canvas.position(530,250);
+
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,360,360)
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modalLoaded);
+    document.getElementById("status").innerHTML="Status = Detecting objects";
+    document.getElementById("object").value;
+}
+
+function modalLoaded(){
+    console.log("Modal Loaded!")
+    status=true;
 }
